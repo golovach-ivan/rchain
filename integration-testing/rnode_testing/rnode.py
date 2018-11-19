e9729754e21f2d1dfae9048ad96ebcfdf5d07aaf
Adam Szkoda <adaszko@gmail.com>
2018-11-19 12:10:55 +0100

    Enable verbose logging by default

--- integration-testing/rnode_testing/rnode.py
+++ integration-testing/rnode_testing/rnode.py
@@ -31,6 +31,14 @@ class NonZeroExitCodeError(Exception):
         self.exit_code = exit_code
         self.output = output
 
+    def __repr__(self):
+        return '{}({}, {}, {})'.format(
+            self.__class__.__name__,
+            self.command,
+            self.exit_code,
+            repr(self.output),
+        )
+
 
 class TimeoutError(Exception):
     def __init__(self, command, timeout):
@@ -145,13 +153,14 @@ class Node:
 
         process = Process(target=execution)
 
-        logging.info(f"{self.name}: Execute '{cmd}'. Timeout: {self.timeout}s")
+        logging.info("container={} command={}".format(self.name, cmd))
 
         process.start()
 
         try:
             exit_code, output = queue.get(self.timeout)
-            logging.info(f"Returning: {exit_code}, '{output}'")
+            logging.info("exit_code={}".format(exit_code))
+            logging.debug('output={}'.format(repr(output)))
             return exit_code, output
         except Empty:
             process.terminate()
